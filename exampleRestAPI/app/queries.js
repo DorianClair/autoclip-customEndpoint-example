a30f4878b950d3335f94457224c21cae5b899b76
Dorian Clair <dorian.clair@agyleintelligence.com>
2021-03-23 20:53:32 -0300

    show

--- exampleRestAPI/app/queries.js
+++ exampleRestAPI/app/queries.js
@@ -73,7 +73,7 @@ const getQuestionsByUserId = (request, response) =>  {
 }
 
 const getQuestionsByManagerId = (request, response) =>  {
-  console.log("userId: " + request.query.managerId);
+  console.log("userId: " + request.query);
   const managerId = request.query.managerId;
   pool.query('SELECT DISTINCT title, name from questions Inner join forms on questions.form_id = forms.id inner join assignedforms on forms.id = assignedforms.form_id Where forms.manager_Id = $1',[managerId], (error, results) => {
     if (error) {
